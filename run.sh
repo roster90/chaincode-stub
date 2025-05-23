f44511aac0da78b6d188c47d0611f32e3a9000fd
Rooster <nguyenthoai06011990@gmail.com>
2025-05-23 11:28:59 +0700

    jupdate

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/token-cc:0.1.3 --push .
+docker build metafi/token-cc:0.1.3  .
 docker push metafi/token-cc:0.1.3
