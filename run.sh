ea0066da4188595b3ab337efcd6549b4fbb03b7a
Rooster <nguyenthoai06011990@gmail.com>
2025-05-23 11:50:02 +0700

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build --platform=linux/amd64 -t metafi/token-cc:0.1.5 .
-docker push metafi/token-cc:0.1.5
+docker build --platform=linux/amd64 -t metafi/token-cc:0.2.0 .
+docker push metafi/token-cc:0.2.0
